b18be6337573b639fc566d451a7baa8911af119a
Adam Kinzer <adamkinzer915@gmail.com>
2015-12-17 13:32:08 -0700

    Added

--- SillyApp/ViewController.swift
+++ SillyApp/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func changeRed(sender: UISlider)
+    {
+        let newRed = CGFloat(Double(sender.value))
+        
+        view.backgroundColor = UIColor(red: newRed, green: CGFloat(0.134), blue: CGFloat(0.745), alpha: 1.0)
+    }
 
-
+    @IBAction func ColorSwitch(sender: UISwitch)
+    {
+        if(sender.on)
+        {
+           view.backgroundColor = UIColor(red: CGFloat(0.999), green: CGFloat(0.134), blue: CGFloat(0.745), alpha: 1.0)
+        }
+        else
+        {
+            view.backgroundColor = UIColor(red: CGFloat(0.001), green: CGFloat(0.134), blue: CGFloat(0.745), alpha: 1.0)
+        }
+    }
+    
+    @IBAction func ColorButton(sender: AnyObject)
+    {
+        let RedColor = CGFloat(arc4random_uniform(4))
+        let BlueColor = CGFloat(arc4random_uniform(2))
+        let GreenColor = CGFloat(arc4random_uniform(3))
+        
+        view.backgroundColor = UIColor(red: RedColor, green: GreenColor, blue: BlueColor, alpha: 1.0)
+    }
+    
 }
 
